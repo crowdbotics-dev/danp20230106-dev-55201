cb57a9709742d2a08648da86d26e0d976efc7124
daniel palacios <daniel.palacios@crowdbotics.com>
2023-01-06 09:55:06 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,3 +1,5 @@
+import { TextInput } from "react-native";
+import { View } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,13 +10,30 @@ const Untitled2 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }} style={styles.ayBEZXKK}><View style={styles.GpRqqlKD}></View><TextInput style={styles.hOTPRGBh}></TextInput></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: '100%',
+    backgroundColor: "#b9dc67"
+  },
+  ayBEZXKK: {
+    backgroundColor: "#eb7d7d"
+  },
+  GpRqqlKD: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#b94242",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  hOTPRGBh: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
   }
 });
 export default Untitled2;
